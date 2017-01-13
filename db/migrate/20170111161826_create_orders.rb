a787765fc6397d0d5bf0f3ef0999c150a12d32a2
Zubry <zubryjs@gmail.com>
2017-01-13 18:56:15 -0500

    Fix migrations?

--- db/migrate/20170111161826_create_orders.rb
+++ db/migrate/20170111161826_create_orders.rb
@@ -1,9 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.string :user
-      t.string :status
-      t.string :restaurant
+      t.belongs_to :user, index: true
+      t.string   "status"
+      t.string   "restaurant"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
 
       t.timestamps
     end
--- db/migrate/20170111213138_create_meals.rb
+++ db/migrate/20170111213138_create_meals.rb
@@ -1,10 +1,12 @@
 class CreateMeals < ActiveRecord::Migration[5.0]
   def change
     create_table :meals do |t|
-      t.string :order
-      t.string :user
-      t.string :name
-      t.string :price
+      t.belongs_to :user, index: true
+      t.string   "order"
+      t.string   "name"
+      t.string   "price"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
 
       t.timestamps
     end
